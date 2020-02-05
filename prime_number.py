a5945ac3d424d5ce715e902348bd8394300928f1
zock3r <aarn.amiri@gmail.com>
2020-02-05 15:15:32 +0330

    make some changes

--- prime_number.py
+++ prime_number.py
@@ -1,8 +1,21 @@
-from math import sqrt
-def prim(n):
-    for i in ceil(sqrt(n))
-        if n % i == 0:
-            return "this is not prime"
+# python program for checking is prime number or not
 
-        else :
-            retrun(5d
+import math
+
+def prime(arg):
+    if arg <= 1 :
+        return False
+    if arg == 2 :
+        return True
+    max_of = math.ceil(math.sqrt(arg))
+    if arg > 2 :
+        for i in range (3 , 1+ max_of):
+            if arg % i == 0:
+
+                return False
+        return True
+prime_func  = prime(22)
+if prime_func == True:
+    print("prime")
+else:
+    print("not prime")
