ae61c733575b8f8578dd2ba57c00590c575f7fc1
zock3r <aarn.amiri@gmail.com>
2020-02-05 22:26:20 +0330

    fixing bugs

--- prime_number.py
+++ prime_number.py
@@ -2,20 +2,26 @@
 
 import math
 
+#make fuction for checking numbers
 def prime(arg):
     if arg <= 1 :
         return False
     if arg == 2 :
         return True
+    if arg % 2 == 0 :
+        return False
     max_of = math.ceil(math.sqrt(arg))
     if arg > 2 :
         for i in range (3 , 1+ max_of):
             if arg % i == 0:
-
                 return False
         return True
-prime_func  = prime(22)
-if prime_func == True:
-    print("prime")
-else:
-    print("not prime")
+
+
+while True:                          #to dont stop loop
+    number = int(input())            #resived number from input 
+    prime_func  = prime(number)      #get fuction
+    if prime_func == True:           #printing function result
+        print("prime")
+    else:
+        print("not prime")
